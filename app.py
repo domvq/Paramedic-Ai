057c04a4504a4ec0c494f9dfe886885c038c08c6
dom <domhasaball@gmail.com>
2026-09-07 14:02:16 -0700

    Clean up unused imports in app.py
    Remove unused imports from app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from pathlib import Path
 import pandas as pd
 import streamlit as st
 import pyttsx3
-import tempfile
-import os
 from streamlit_mic_recorder import speech_to_text
 
 from src.ml_model import (
